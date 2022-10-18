b4a818f69ef494429dfb38b83a859c45c9b3bfd6
Alex P from nllab_ot2 <a.j.p.perkins@sms.ed.ac.uk>
2022-10-18 19:11:03 +0100

    general update

--- Technical_error_active_learning/src/OT2_scripts/ALTE009/experiment_design_to_json_parser.py
+++ Technical_error_active_learning/src/OT2_scripts/ALTE009/experiment_design_to_json_parser.py
@@ -97,10 +97,16 @@ master_plate_pipetting_settings_dict = {}
 # initialise empty a master_plate_experiment_settings_dict
 master_plate_experiment_settings_dict = {}
 
+# initialise empty a pre_experiment_compilation_dict
+pre_experiment_compilation_dict = {}
 
 # slice the df to deal with one plate at a time
 for plate in range(1, plates_required+1,1):
+
+
     single_plate_df = experiment_design_df[experiment_design_df['Plate'] == plate]
+
+
 
     # initialise the plate_pipetting_settings_dict
     plate_pipetting_settings_dict = {}
@@ -109,7 +115,10 @@ for plate in range(1, plates_required+1,1):
     plate_experiment_settings_dict = {}
 
     # iterate over the runs of single_plate_df:
-    for i,run in single_plate_df.iterrows():
+    for index, runner in enumerate(range(0,single_plate_df.shape[0],1)):
+
+        run = single_plate_df.iloc[index,:]
+  
 
         # pipetting_settings:
 
@@ -133,10 +142,99 @@ for plate in range(1, plates_required+1,1):
         run_experiment_settings_dict = base_experiment_settings_dict.copy()
         # use the run_name to assign the dispense well.
         run_experiment_settings_dict['dispense_well_list'] = [run_name]
+
+        ### attaching the correct substrates_source_well
+        ### each 200ul PCR tube can supply 25x reactions. So progress every 24 (1x row of nunc plate)
+        # add one because of python indexing
+        experiment_number = index + 1
+
+        if experiment_number <= 24:
+            run_experiment_settings_dict['substrates_source_well'] = "A1"
+            run_experiment_settings_dict['lysate_source_well'] = "B1"
+
+        elif experiment_number <= 48:
+            run_experiment_settings_dict['substrates_source_well'] = "A2"
+            run_experiment_settings_dict['lysate_source_well'] = "B2"
+
+        elif experiment_number <= 72:
+            run_experiment_settings_dict['substrates_source_well'] = "A3"
+            run_experiment_settings_dict['lysate_source_well'] = "B3"
+
+        elif experiment_number <= 96:
+            run_experiment_settings_dict['substrates_source_well'] = "A4"
+            run_experiment_settings_dict['lysate_source_well'] = "B4"
+
+        elif experiment_number <= 120:
+            run_experiment_settings_dict['substrates_source_well'] = "A5"
+            run_experiment_settings_dict['lysate_source_well'] = "B5"
+
+        elif experiment_number <= 144:
+            run_experiment_settings_dict['substrates_source_well'] = "A6"
+            run_experiment_settings_dict['lysate_source_well'] = "B6"
+
+        elif experiment_number <= 168:
+            run_experiment_settings_dict['substrates_source_well'] = "A7"
+            run_experiment_settings_dict['lysate_source_well'] = "B7"
+
+        elif experiment_number <= 192:
+            run_experiment_settings_dict['substrates_source_well'] = "A8"
+            run_experiment_settings_dict['lysate_source_well'] = "B8"
+
+        elif experiment_number <= 216:
+            run_experiment_settings_dict['substrates_source_well'] = "A9"
+            run_experiment_settings_dict['lysate_source_well'] = "B9"
+
+        elif experiment_number <= 240:
+            run_experiment_settings_dict['substrates_source_well'] = "A10"
+            run_experiment_settings_dict['lysate_source_well'] = "B10"
+
+        elif experiment_number <= 264:
+            run_experiment_settings_dict['substrates_source_well'] = "A11"
+            run_experiment_settings_dict['lysate_source_well'] = "B12"
+
+        elif experiment_number <= 288:
+            run_experiment_settings_dict['substrates_source_well'] = "A12"
+            run_experiment_settings_dict['lysate_source_well'] = "B12"
+
+        elif experiment_number <= 312:
+            run_experiment_settings_dict['substrates_source_well'] = "A13"
+            run_experiment_settings_dict['lysate_source_well'] = "B13"
+
+        elif experiment_number <= 336:
+            run_experiment_settings_dict['substrates_source_well'] = "A14"
+            run_experiment_settings_dict['lysate_source_well'] = "B14"
+
+        elif experiment_number <= 360:
+            run_experiment_settings_dict['substrates_source_well'] = "A15"
+            run_experiment_settings_dict['lysate_source_well'] = "B15"
+
+        elif experiment_number <= 384:
+            run_experiment_settings_dict['substrates_source_well'] = "A16"
+            run_experiment_settings_dict['lysate_source_well'] = "B16"
+
+
+
+
+
+
         # save the run_experiment_settings as an entry of the plate_experiment_settings_dict with the run_name as it's key
         plate_experiment_settings_dict[run_name] = run_experiment_settings_dict
 
+    #asdf
+    substrate_source_list_possibles = ["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12", "A13", "A14", "A15", "A16"]
+    lysate_source_list_possibles = ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12", "B13", "B14", "B15", "B16"]
 
+    platewise_pre_experiment_compliation_dict = {
+        "substrate_source_tubes_required": round(experiment_number/24),
+        "substrate_source_tubes_list": substrate_source_list_possibles[0:round(experiment_number/24)],
+        "substrate_source_volume": 200,
+        "lysate_source_tubes_required": round(experiment_number/24),
+        "lysate_source_tubes_list": lysate_source_list_possibles[0:round(experiment_number/24)],
+        "lysate_source_volume": 75
+    }
+
+    # store the number of substrate source tubes needed
+    pre_experiment_compilation_dict[plate] = platewise_pre_experiment_compliation_dict
 
     # save the plate_pipetting_settings_dict as an entry of the master_plate_pipetting_settings_dict with the plate as it's key
     master_plate_pipetting_settings_dict[plate] = plate_pipetting_settings_dict
@@ -172,3 +270,11 @@ for plate in range(1, plates_required+1,1):
 experiment_design_df = experiment_design_df.iloc[:,1:]
 # save the updated design
 experiment_design_df.to_csv("processed_data_files/design_real_assigned.csv", index=False)
+
+
+# save the pre_experiment_compilation_dict
+
+# use the experiment_prefix and the plate number to name the file
+pre_experiment_filename = experiment_prefix +"_pre_experiment_compilations.json"
+with open(pre_experiment_filename, 'w') as fp:
+    json.dump(pre_experiment_compilation_dict, fp)

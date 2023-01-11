73c1b157f614d6417738d2cde8bfb9286a12dc2e
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-01-11 11:50:55 +0000

    added auxillary scripts directory with dilution calculator func

---
+++ Technical_error_active_learning/src/OT2_scripts/ALTE010/auxillary_scripts/dilution_calculator.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def dilution_calculator(stock_concentration, final_total_volume, final_concentration):
+
+    return (final_total_volume * final_concentration) / stock_concentration

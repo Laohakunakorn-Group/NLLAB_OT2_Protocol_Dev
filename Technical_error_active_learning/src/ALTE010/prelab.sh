b768e8d97632dcfc1eb225d9a05e9525777194f9
Alex Perkins <s1503400@ed.ac.uk>
2023-05-11 18:33:49 +0100

    absolute paths

--- Technical_error_active_learning/src/ALTE010/prelab.sh
+++ Technical_error_active_learning/src/ALTE010/prelab.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-if [ -d "output/" ]; then
-rm -r output/
+if [ -d "/app/output/" ]; then
+rm -r /app/output/
 else
 :
 fi
 
-mkdir output/
-mkdir output/Experiment_Designs/
-mkdir output/MasterMixes
-mkdir output/Instructions
+mkdir /app/output/
+mkdir /app/output/Experiment_Designs/
+mkdir /app/output/MasterMixes
+mkdir /app/output/Instructions
 
 
 if [ -d "tmp/" ]; then
@@ -22,7 +22,7 @@ mkdir tmp/
 mkdir tmp/MasterMixes/
 mkdir tmp/Experiment_Designs/
 
-R < 1_doe.r --no-save
-python3 2_mastermixes_count.py
-python3 3_mastermixes_assign.py
-python3 4_mastermix_calculations.py
+R < "/app/prelab_scripts/1_doe.r" --no-save
+python3 "/app/prelab_scripts/2_mastermixes_count.py"
+python3 "/app/prelab_scripts/3_mastermixes_assign.py"
+python3 "/app/prelab_scripts/4_mastermix_calculations.py"

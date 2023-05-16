f69ac72314ffea9239e9e77c453595cd70da6c78
Alex Perkins <s1503400@ed.ac.uk>
2023-05-16 22:05:33 +0100

    initialised RSM analysis

--- Technical_error_active_learning/src/ALTE010/analysis_scripts/a3_analysis.py
+++ Technical_error_active_learning/src/ALTE010/analysis_scripts/a3_analysis.py
@@ -30,31 +30,3 @@ bar_plot_data = fdbkp_experiment[fdbkp_experiment["Time"] == 300]
 # round data 
 bar_plot_data.loc[:, "GFP_uM"] = bar_plot_data.loc[:, "GFP_uM"].round(2)
 
-bar_plot_data = bar_plot_data.sort_values(by='GFP_uM', ascending=False)
-print(bar_plot_data)
-print(bar_plot_data.columns)
-
-fig = plt.figure(figsize=(10,5))
-fig.suptitle("GFP Signal vs ORF Length")
-sns.scatterplot(data = bar_plot_data, y = "GFP_uM", x = "ORF_Length", hue = "DNA_Template")
-plt.savefig("/app/analysis_output/plots/GFP Signal_vs_ORF_Length.png")
-
-
-fig = plt.figure(figsize=(10,5))
-fig.suptitle("GFP Signal vs Pre-GFP GC%")
-sns.scatterplot(data = bar_plot_data, x = "GFP_uM", y = "ORF_GC%", hue = "DNA_Template")
-plt.savefig("/app/analysis_output/plots/GFP_Signal_vs_Pre_GFP_GC%.png")
-
-
-## just peg
-
-peg_data = tidy_data[tidy_data["DNA_Template"] == "T7_GFP"]
-print(peg_data["Energy_Solution"].unique())
-print(peg_data["DNA_Template"].unique())
-
-fig = plt.figure(figsize=(10,5))
-fig.suptitle("PEG_8000% vs GFP_Signal")
-ax = sns.barplot(data = peg_data, x = "Energy_Solution", y = "GFP_uM")
-ax.set(xlabel='PEG-8000', ylabel='GFP_uM')
-ax.set_xticklabels(["4%", "2%" ])
-plt.savefig("/app/analysis_output/plots/PEG_8000%_vs_GFP_Signal.png")

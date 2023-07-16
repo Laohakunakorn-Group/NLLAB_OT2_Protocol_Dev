a3da651ac28ee72ddb4c4908b365cade4983a362
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-07-16 01:57:47 +0100

    finished well metadata generator

--- Technical_error_active_learning/src/Manual_CCD_Mg_K_DTT_T7_GFP/analysis_scripts/1_generate_well_metadata_json.py
+++ Technical_error_active_learning/src/Manual_CCD_Mg_K_DTT_T7_GFP/analysis_scripts/1_generate_well_metadata_json.py
@@ -1,28 +1,37 @@
 import pandas as pd 
 import json
 
+def generate_well_metadata_dict():
+    """
+    produces the well dict by reading the design and adding the template metadata.
+    """
+    
+    # import the template and design
+    template_metadata = json.load(open("/app/analysis_scripts/template_well_metadata.json"))
+    design_df = pd.read_csv("/app/analysis_scripts/design.csv")
 
-template_metadata = json.load(open("/app/analysis_scripts/template_well_metadata.json"))
+    # initialise the final dict
+    well_metadata_dict = {}
 
-design_df = pd.read_csv("/app/analysis_scripts/design.csv")
+    #iterate over the rows (wells) in the design
+    for i, well in design_df.iterrows():
 
-print(template_metadata)
-print(type(template_metadata))
+        # initalise the well dict
+        well_dict = {}
 
-print("")
+        # extract and store all the template metadata in the well dict
+        for key, value in template_metadata["Template"].items():
+            well_dict[key] = value
 
+        # store the well relevant metrics
+        well_dict["MG_Glut"] = well["MG_Glut"]
+        well_dict["K_Glut"] = well["K_Glut"]
+        well_dict["DTT"] = well["DTT"]
 
-well_metadata_dict = {}
+        # store the whole thing under the well code
+        well_metadata_dict[well["Well"]] = well_dict
+    
+    return well_metadata_dict
 
-for i, well in design_df.iterrows():
-
-    well_dict = template_metadata
-
-    well_dict["MG_Glut"] = well["MG_Glut"]
-    well_dict["K_Glut"] = well["K_Glut"]
-    well_dict["DTT"] = well["DTT"]
-
-    well_metadata_dict[well["Well"]] = well_dict
-
-
-print(well_metadata_dict)
+#    with open("/app/ana.json", "w") as outfile:
+#        json.dump(well_metadata_dict, outfile)
